37900bc381d27a0d85fb83cf90536561feaeaf79
manuhostin <hostimmanu@gmail.com>
2025-06-03 08:32:38 -0300

    fix: correcao no codigo

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,4 +2,5 @@ from .user import UserSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .autor import AutorSerializer
-from .livro import LivroListRetrieveSerializer, LivroSerializer, LivroListSerializer
+from .livro import LivroListSerializer,LivroRetrieveSerializer, LivroSerializer
+
--- core/views/livro.py
+++ core/views/livro.py
@@ -3,10 +3,6 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import Livro
 from core.serializers import LivroListSerializer, LivroRetrieveSerializer, LivroSerializer
 
-
-
-
-
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
